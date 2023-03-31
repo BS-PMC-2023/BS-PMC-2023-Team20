4d39e1e12562580338fc59e81bbbef226b7093f8
kiliman1995@gmail.com <kiliman1995@gmail.com>
2023-03-31 13:25:07 +0300

    add logo of the site

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import './Navbar.css'
-
+import { ReactComponent as SCEicon } from "../../Assets/SCEicon.svg";
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
 <section id='Navbar'  className='Navbar'>
-        
-</section>
+    <header className="header flex">
+        <div className="logoDiv">
+            <Link to="/" className="logo">
+                    <h1><SCEicon className="icon" />Warehouse.</h1>
+            </Link>
+        </div>
 
+        
+    </header>
+
+
+
+</section>
 )
-  
 }
 
 export default Navbar
