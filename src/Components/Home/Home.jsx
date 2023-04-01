2c40b86e6142da1fcd1282815162a25810e00f51
Daniel Markov <dani33339@gmail.com>
2023-04-01 15:01:01 +0300

    BSPMC2320-30: Home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,12 +6,31 @@ import Main from '../Main/Main';
 
 
 const Home = () => {
-    return (
-<section id='home'  className='home'>
-        
-</section>
 
-)
+    useEffect(()=>{
+        Aos.init({duration: 2000})
+      }, [])
+    
+    return (
+    
+        <><section id='home' className='home'>
+          <div className="overlay"></div>
+    
+          <div data-aos="fade-down" className="homeContent container">
+            <div className="textDiv">
+              <span className="smallText">
+                Our Warehouse
+              </span>
+
+              
+            </div>
+            
+          </div>
+    
+          
+        </section><Main /></> 
+        
+      )
   
 }
 
