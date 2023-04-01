9a1168f695eb411328827c63688fa5123d9259fb
Daniel Markov <dani33339@gmail.com>
2023-04-01 15:07:36 +0300

    BSPMC2320-30: import data from DB

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -23,14 +23,52 @@ const Main = () => {
         const data = await getDocs(ItemsRef) 
         setItems(data.docs.map((doc) => (doc.data())));
       }
-    
+
     return (
         <section id='main' className='main section container'>
         <div className="secTitle">
           <h3 className="title">
             Available items
           </h3>
-        </div>        
+        </div>   
+
+         <div className="secContent grid">
+        {
+          Items.map((item,index) => {
+            return (
+              
+              <div key={index} data-aos="fade-up" className="singleDestination">
+        
+              <div className="imageDiv">
+              <img src={item.ImageUrl} alt="" />
+              </div>
+   
+             <div className="cardInfo">
+              <span className="continent flex">
+                 <HiOutlineLocationMarker className="icon"/>
+                 <span className="name">location: {item.Location}</span>
+              </span>
+   
+              <div className="fees flex">
+                 <div className="grade ">
+
+                   <span  className="textD ">Serial </span>
+                   <span>{item.Serial} </span>
+                 </div>  
+                  
+              </div>
+                
+                <div className="desc">
+               <p>Description: {item.Description}</p>
+              </div>
+                 <button className='btn flex'  >Order <HiClipboardList className="icon"/> </button>
+                </div>
+              </div>
+      
+            )
+          }) 
+        }
+      </div>     
 </section>
 
 )
