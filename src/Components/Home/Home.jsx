2cd22affb9767b3edb508a4e5fe20cf4120175f0
kiliman1995@gmail.com <kiliman1995@gmail.com>
2023-04-02 00:24:40 +0300

    add && design video of Visual Communication on the home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import Main from '../Main/Main';
-
+import video from "../../Assets/video3.mp4";
 
 const Home = () => {
 
@@ -15,7 +15,7 @@ const Home = () => {
     
         <><section id='home' className='home'>
           <div className="overlay"></div>
-    
+          <video src={video} autoPlay loop muted type="video/mp4"></video>
           <div data-aos="fade-down" className="homeContent container">
             <div className="textDiv">
               <span className="smallText">
