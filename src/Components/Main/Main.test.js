8f4afb9eb9574b6550af1e35111258321c18422f
Daniel Markov <dani33339@gmail.com>
2023-04-07 20:31:50 +0300

    delete import in main

--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -1,7 +1,6 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Main from './Main';
-import { toBeInTheDocument } from '@testing-library/jest-dom/extend-expect'; // import the method
 
 describe('Main component', () => {
   test('renders "Available items" section title', () => {
