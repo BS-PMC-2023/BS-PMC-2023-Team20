7e7823226ff8c4cb33154e232d7a5bee2edb6770
Daniel Markov <dani33339@gmail.com>
2023-04-29 22:22:50 +0300

    BSPMC2320-109: data pass from main to order

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,7 +1,24 @@
 import './Order.css'
+import React, {useEffect,useState} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import Aos from 'aos'
+import { auth } from "../../firebase-config";
 
 
 const Order = () => {
+    const { state: item } = useLocation();
+    const [user, loading] = useAuthState(auth);
+    const [FromDate, setFromDate] = useState("");
+    const [ReturnDate, setReturnDate] = useState("");
+    
+    useEffect(()=>{
+      Aos.init({duration: 4000})
+  
+    console.log(item)
+    }, [user])
+
+
     return (
 <section id='Order'  className='Order'>
         
