c35b71c82fd0c93f9f72b11374eb81452e3efd6c
Daniel Markov <dani33339@gmail.com>
2023-04-29 22:39:35 +0300

    BSPMC2320-109 : handleSubmit function

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -5,6 +5,10 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import Aos from 'aos'
 import { auth } from "../../firebase-config";
 import {HiOutlineLocationMarker} from 'react-icons/hi'
+import {doc, getDoc, serverTimestamp, setDoc, updateDoc} from "firebase/firestore";
+import { uid } from 'uid'
+import { db,} from "../../firebase-config";
+
 
 
 
@@ -22,37 +26,75 @@ const Order = () => {
 
     let navigate = useNavigate();
 
-    const handleSubmit = async () => {
+    const sleep = ms => new Promise(
+        resolve => setTimeout(resolve, ms)
+      );
+      
+      const handleSubmit = async () => {
+        const getuser = doc(db, 'users', user.uid);
+        const data = await getDoc(getuser);
+        var userData = data.data();
+    
+        const getitem = doc(db, 'items', item.uuid);
+    
+        var itemWithoutreservations = Object.assign({}, item);
+        delete itemWithoutreservations.reservations; 
+    
+        var ruid = uid();
+        await setDoc(doc(db, "reservations", ruid), {
+          FirstName: userData.FirstName,
+          LastName: userData.LastName,
+          Userid: user.uid,
+          Itemid: item.uuid,
+          FromDate: FromDate,
+          ReturnDate: ReturnDate,
+          timeStamp: serverTimestamp(),
+        });
+    
+        await updateDoc(getitem, {
+          reservations: [...item.reservations, {"FirstName":userData.FirstName,"LastName":userData.LastName,"ruid":ruid}]
+        });
+    
+    
+        await updateDoc(getuser, {
+          reservations: [...userData.reservations, ruid]
+        });
+    
+    
+            
+            await updateDoc(getuser, {
+            });
+        await sleep(1000);
         navigate("/Myorders");
-    }
+      }
 
     return (
-        <section id='main' className='main section container'>
-        <div className="secTitle">
-          <h3 className="title">
-            Order item now
-          </h3>
-        </div>
+         <section id='main' className='main section container'>
+    <div className="secTitle">
+      <h3 className="title">
+        Order item now
+      </h3>
+    </div>
   
-        <div className="secContent grid">        
-                  <div className="imageDiv">
-                  <img src={item.ImageUrl} alt="" />
-                  </div>
+    <div className="secContent grid">        
+      <div className="imageDiv">
+        <img src={item.ImageUrl} alt="" />
+      </div>
       
-                <div className="cardInfo">
-                  <h4 className="Title"> {item.ItemType}</h4>
-                  <span className="continent flex">
-                    <HiOutlineLocationMarker className="icon"/>
-                    <span className="name">Location: {item.Location}</span>
-                  </span>
+      <div className="cardInfo">
+        <h4 className="Title"> {item.ItemType}</h4>
+        <span className="continent flex">
+          <HiOutlineLocationMarker className="icon"/>
+          <span className="name">Location: {item.Location}</span>
+        </span>
   
-                  <div className="Sirel">
-                  <p>Serial: {item.Serial}</p>
-                  </div>
+        <div className="Sirel">
+          <p>Serial: {item.Serial}</p>
+        </div>
         
-                  <div className="desc">
-                  <p>Description: {item.Description}</p>
-                  </div>
+        <div className="desc">
+          <p>Description: {item.Description}</p>
+        </div>
   
   
           <div >         
@@ -74,24 +116,18 @@ const Order = () => {
                 </div>
               </div>
                
-                </form>
-  
-                  
+                </form>             
               <button  className="btn">
               <a onClick={ () => {  
                 handleSubmit();
                 }}>Submit</a> 
             </button> 
         </div>
-                  </div>              
+             </div>              
             </div>
           </div>
-         
-      </section>
-      
-
-)
-  
+      </section>     
+)  
 }
 
 export default Order
