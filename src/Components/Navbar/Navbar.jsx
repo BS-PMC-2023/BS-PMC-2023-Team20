be7e8386919dd3acd78be4c0d923c65e7dcd4f6b
kiliman1995@gmail.com <kiliman1995@gmail.com>
2023-04-29 17:18:59 +0300

    fix the style of the rest button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -38,14 +38,6 @@ const Navbar = () => {
     };
 
     return (
-        <section id='Navbar'  className='Navbar'>
-            <header className="header flex">
-                <div className="logoDiv">
-                    <Link to="/" className="logo">
-                            <h1><SCEicon className="icon" />Warehouse.</h1>
-                    </Link>
-                </div>
-                <div className={active}>
 <section id='Navbar'  className='Navbar'>
     <header className="header flex">
         <div className="logoDiv">
