685d515ca184ebada19e81ed4e0c7c6377e79a00
Daniel Markov <dani33339@gmail.com>
2023-04-30 23:05:32 +0300

    BSPMC2320-96: added component for terms

---
+++ src/Components/Terms/Terms.jsx
@@ -0,0 +1,11 @@
+
+const Terms = () => {
+  return (
+    <div className="terms">
+      <h2>Terms and Conditions</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id velit quis erat ultricies consequat. Donec in finibus nulla. Integer condimentum dolor non lorem rhoncus, eget semper purus sollicitudin.</p>
+    </div>
+  );
+};
+
+export default Terms
