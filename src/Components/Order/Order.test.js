bd94454020113c7d4a9a04f8fa95477aff8ad535
Orel Maier <orelmaier@gmail.com>
2023-05-09 20:34:36 +0300

    BSPMC2320-155 added unit test for ddl

--- src/Components/Order/Order.test.js
+++ src/Components/Order/Order.test.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import Order from './Order';
+
 const { Builder, By, until, Actions } = require('selenium-webdriver');
 const assert = require('assert');
 
@@ -23,7 +29,21 @@ describe('Order component', () => {
      
        const url = await driver.getCurrentUrl();
        expect(url).toContain("/Order"); 
-});
+    });
 
+    //added to check if there is ddl for the number that selected
+    test('renders drop-down list on changing the number of group members', () => {
+      render(<Order />);
+  
+      // Find the select element for the number of group members
+      const groupMembersSelect = screen.getByLabelText(/Number of group members/i);
+  
+      // Change the value of the select element to 3
+      userEvent.selectOptions(groupMembersSelect, '3');
+  
+      // Check if the drop-down list for the 2nd and 3rd members are visible
+      expect(screen.getByLabelText(/Email of 2 member/i)).toBeInTheDocument();
+      expect(screen.getByLabelText(/Email of 3 member/i)).toBeInTheDocument();
+    });
 });
 
