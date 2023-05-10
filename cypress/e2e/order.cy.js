8cf31a1e9b1b5f8c090e38c30f76dbdc183c2def
Daniel Markov <dani33339@gmail.com>
2023-05-10 13:09:49 +0300

    TEST26

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -11,24 +11,24 @@ describe('Order component', () => {
     cy.xpath('/html/body/div/div/section/div[2]/form/div[3]/span').click();
     cy.wait(4000); // Wait for one second
   });
-    it('clicking the Order button should navigate to the Order page,set dates should display number of rent days,test that user have to check box before submit,test that user reservation is submitted', () => {
-      cy.visit('http://localhost:3000/');
-      cy.xpath('/html/body/div/div/section[2]/div[2]/div/div[2]/button').click();
-      cy.url().should('include', '/Order');
-      cy.get('.DepartInput input').click();
-      cy.get('.DepartInput input').type('2023-09-06');
-      cy.get('.ReturnInput input').click();
-      cy.get('.ReturnInput input').type('2023-09-13');
-      cy.wait(1000); // Wait for the element to appear
-      cy.xpath('/html/body/div/div/section/div[2]/div[2]/div[3]/div/form/div[3]/p').should('contain', '7');
-      cy.contains('Submit').click();
-      cy.on('window:alert', (str) => {
-      expect(str).to.equal('Please accept the terms and conditions');
-      // cy.get('label > span').click();
-      // cy.contains('Submit').click();
-      // cy.url().should('include', '/Myorders');
-      });
-    });
+    // it('clicking the Order button should navigate to the Order page,set dates should display number of rent days,test that user have to check box before submit,test that user reservation is submitted', () => {
+    //   cy.visit('http://localhost:3000/');
+    //   cy.xpath('/html/body/div/div/section[2]/div[2]/div/div[2]/button').click();
+    //   cy.url().should('include', '/Order');
+    //   cy.get('.DepartInput input').click();
+    //   cy.get('.DepartInput input').type('2023-09-06');
+    //   cy.get('.ReturnInput input').click();
+    //   cy.get('.ReturnInput input').type('2023-09-13');
+    //   cy.wait(1000); // Wait for the element to appear
+    //   cy.xpath('/html/body/div/div/section/div[2]/div[2]/div[3]/div/form/div[3]/p').should('contain', '7');
+    //   cy.contains('Submit').click();
+    //   cy.on('window:alert', (str) => {
+    //   expect(str).to.equal('Please accept the terms and conditions');
+    //   // cy.get('label > span').click();
+    //   // cy.contains('Submit').click();
+    //   // cy.url().should('include', '/Myorders');
+    //   });
+    // });
   
     // it('set dates should display number of rent days', () => {
     //   cy.get('.DepartInput input').click();
