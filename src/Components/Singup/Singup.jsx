f094b1948033409bb9ac78a9122397e552a1c8cb
kiliman1995@gmail.com <kiliman1995@gmail.com>
2023-05-21 11:58:30 +0300

    BSPMC2320-212: להראות הודעת שגיאה מתאימה אם מנסים להתחבר ממייל שללא של המכללה

--- src/Components/Singup/Singup.jsx
+++ src/Components/Singup/Singup.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import './Singup.css'
 import video from "../../Assets/video3.mp4";
-import { BiUserCircle } from 'react-icons/bi'
-import { RiLockPasswordFill } from 'react-icons/ri'
-import { AiOutlineFileDone } from 'react-icons/ai'
-import { MdDriveFileRenameOutline } from 'react-icons/md'
+import {BiUserCircle} from 'react-icons/bi'
+import {RiLockPasswordFill} from 'react-icons/ri'
+import {AiOutlineFileDone} from 'react-icons/ai'
+import {MdDriveFileRenameOutline} from 'react-icons/md'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import { useNavigate } from 'react-router-dom'
@@ -15,59 +15,63 @@ import {
   setDoc,
 } from "firebase/firestore";
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../../firebase-config";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth,db } from "../../firebase-config";
 
 
 const Singup = () => {
-  useEffect(() => {
-    Aos.init({ duration: 2000 })
+  useEffect(()=>{
+    Aos.init({duration: 2000})
   }, [])
-
+  
   const [registerEmail, setRegisterEmail] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
   const [FirstName, setFirstName] = useState("");
   const [LastName, setLastName] = useState("");
   const [birthday, setbirthday] = useState("");
   const userRoles = ['user'];
-
+  
   let navigate = useNavigate();
-
+  
   const register = async () => {
     try {
+      if( CheckEmail()){
+        throw new Error('You must enter SCE email!');
+      }
       const user = await createUserWithEmailAndPassword(
         auth,
         registerEmail,
         registerPassword
       )
-      await setDoc(doc(db, "users", user.user.uid), {
-        email: registerEmail,
-        FirstName: FirstName,
-        LastName: LastName,
-        birthday: birthday,
-        timeStamp: serverTimestamp(),
-        userRoles,
-        reservations: [],
-        card: [],
-      });
-      const UserRef = doc(db, "users", user.user.uid);
-      const data = await getDoc(UserRef);
-      localStorage.setItem("user", JSON.stringify(data.data()));
-      navigate("/");
-      window.location.reload(false);
+        await setDoc(doc(db, "users", user.user.uid), {
+          email :registerEmail, 
+          FirstName :FirstName,
+          LastName :LastName,
+          birthday:birthday ,
+          timeStamp: serverTimestamp(),
+          userRoles,
+          reservations : [],
+          card :[],
+        });
+        const UserRef = doc(db, "users", user.user.uid);
+        const data = await getDoc(UserRef);
+        localStorage.setItem("user", JSON.stringify(data.data()));
+        navigate("/");
+        window.location.reload(false);
     } catch (error) {
       console.log(error.message);
-      alert("Error somthing went wrong please try again " + " " + error.message);
+      alert("Error somthing went wrong please try again " + " "+error.message); 
     }
   };
 
   // sce email cheking
-  const CheckEmail = async () => {
+  const CheckEmail = async() =>{
     const regex = /[a-zA-Z0-9]+@ac.sce.il/g
-    if (!regex.test(registerEmail)) {
+    if(!regex.test(registerEmail)){
       return true;
     }
-    else {
+    else
+    {
       return false;
     }
   }
@@ -79,70 +83,70 @@ const Singup = () => {
 
       <div data-aos="fade-down" className="Sing-upContent container">
         <div className="textDiv">
-          <span className="smallText">
-            Sing-up Page
-          </span>
-          <h1 data-aos="fade-down" className="Sing-upTitle">
-            Create Account right now
-          </h1>
+        <span  className="smallText">
+          Sing-up Page
+        </span>
+        <h1 data-aos="fade-down" className="Sing-upTitle">
+          Create Account right now
+        </h1>
         </div>
 
-        <form data-aos="fade-down" className="cardDiv grid" onSubmit={register}>
+        <form data-aos="fade-down" className="cardDiv grid" onSubmit = {register}>
 
           <div className="emailInput">
             <label htmlFor="emailName">Enter your email:</label>
             <div className="input flex">
-              <input type="text" placeholder='Enter email here...' onChange={(event) => {
-                setRegisterEmail(event.target.value);
-
-              }} />
-              <BiUserCircle className="icon" />
+            <input type="text" placeholder='Enter email here...' onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          
+          }}/>
+            <BiUserCircle className="icon"/>
             </div>
           </div>
 
           <div className="PassWordInput">
             <label htmlFor="PassWord">Enter your password:</label>
             <div className="input flex">
-              <input type="password" placeholder='Enter password here...' onChange={(event) => {
-                setRegisterPassword(event.target.value);
-              }} />
-              <RiLockPasswordFill className="icon" />
+            <input type="password"  placeholder='Enter password here...'  onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}/>
+            <RiLockPasswordFill className="icon"/>
             </div>
           </div>
 
           <div className="FnameInput">
             <label htmlFor="Fname">Enter your First name:</label>
             <div className="input flex">
-              <input type="text" placeholder='Enter First name here...' onChange={(event) => {
-                setFirstName(event.target.value);
-              }} />
-              <MdDriveFileRenameOutline className="icon" />
+            <input type="text" placeholder='Enter First name here...' onChange={(event) => {
+            setFirstName(event.target.value);
+          }}/>
+            <MdDriveFileRenameOutline className="icon"/>
             </div>
           </div>
 
           <div className="LnameInput">
             <label htmlFor="Lname">Enter your Last name:</label>
             <div className="input flex">
-              <input type="text" placeholder='Enter Last name here...' onChange={(event) => {
-                setLastName(event.target.value);
-              }} />
-              <MdDriveFileRenameOutline className="icon" />
+            <input type="text" placeholder='Enter Last name here...' onChange={(event) => {
+            setLastName(event.target.value);
+          }}/>
+            <MdDriveFileRenameOutline className="icon"/>
             </div>
           </div>
 
           <div className="dateInput">
             <label htmlFor="date">Select your birthday date :</label>
             <div className="input flex">
-              <input type="date" onChange={(event) => {
-                setbirthday(event.target.value);
-              }} />
+            <input type="date" onChange={(event) => {
+            setbirthday(event.target.value);
+          }}/>
             </div>
           </div>
 
 
           <div className="submit flex">
-            <AiOutlineFileDone className="icon" />
-            <span type="submit" onClick={register} >Submit</span>
+           <AiOutlineFileDone className="icon"/>
+           <span type="submit" onClick={register} >Submit</span>
           </div>
 
         </form>
