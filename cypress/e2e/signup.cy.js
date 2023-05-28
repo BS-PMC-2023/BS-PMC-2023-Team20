b318220a481c580b78cbb2b97d8c2f3ba4c26edf
p4vel <versiontv1@gmail.com>
2023-05-28 20:37:04 +0300

    Signup intergration test

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,34 @@
+describe('Signup Page', function() {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/Sing-up');
+    });
+  
+    it('Allows a new user to sign up', function() {
+      cy.get('.emailInput input').type('testIntergration@example.com');
+      cy.get('.PassWordInput input').type('testpassword');
+      cy.get('.FnameInput input').type('Test');
+      cy.get('.LnameInput input').type('User');
+      cy.get('.dateInput input').type('2000-01-01');
+  
+      cy.get('.submit span').click();
+  
+      cy.url().should('eq', 'http://localhost:3000/');
+  
+      cy.reload();
+      cy.window().its('localStorage.user').should('exist');
+    });
+  
+    it('Shows an error if the email is already in use', function() {
+      cy.get('.emailInput input').type('admin@example.com');
+      cy.get('.PassWordInput input').type('testpassword');
+      cy.get('.FnameInput input').type('Test');
+      cy.get('.LnameInput input').type('User');
+      cy.get('.dateInput input').type('2000-01-01');
+  
+      cy.get('.submit span').click();
+  
+      cy.get('.error-notification').should('be.visible');
+      cy.get('.error-bubble').should('contain', 'This email is already in use. Please try another email.');
+    });
+  });
+  
