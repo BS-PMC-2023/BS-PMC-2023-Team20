17869c07eab8a1ed10cbe6a099f72fbec7b0ddfe
p4vel <versiontv1@gmail.com>
2023-06-05 16:55:59 +0300

    BSPMC2320-261 confirmation test

--- cypress/e2e/confirmation.cy.js
+++ cypress/e2e/confirmation.cy.js
@@ -6,7 +6,7 @@ describe("Confirmation Page", () => {
   
     it('should allow users to sign in with valid credentials', () => {
         cy.visit('http://localhost:3000/Sign-in');
-        cy.get('input[type="text"]').type('admin@gmail.com');
+        cy.get('input[type="text"]').type('admin@ac.sce.il');
         cy.get('input[type="password"]').type('123456');
         cy.xpath('/html/body/div/div/section/div[2]/form/div[3]/span').click();
         cy.wait(4000); // Wait for four seconds
@@ -14,7 +14,7 @@ describe("Confirmation Page", () => {
       });
     it("renders confirmation page with correct reservation data", () => {
         cy.visit('http://localhost:3000/Sign-in');
-        cy.get('input[type="text"]').type('admin@gmail.com');
+        cy.get('input[type="text"]').type('admin@ac.sce.il');
         cy.get('input[type="password"]').type('123456');
         cy.xpath('/html/body/div/div/section/div[2]/form/div[3]/span').click();
         cy.wait(4000); // Wait for four seconds
@@ -34,19 +34,20 @@ describe("Confirmation Page", () => {
       cy.get("@reservationCard").should("contain", "Status:");
   
       //Click the "Set as Pending" button and verify the status changes
-      cy.get("@reservationCard").find(".btn").contains("Set as Pending").click();
+      cy.wait(1000);  // Wait for one second
+      cy.get("@reservationCard").find(".status-buttons .orange").click({force: true});
       cy.get("@reservationCard").should("contain", "Status: Pending");
       //Click the "Cancel" button and verify the status changes
-      cy.get("@reservationCard").find(".btn").contains("Cancel").click();
+      cy.get("@reservationCard").find(".status-buttons .red").click({force: true});
       cy.get("@reservationCard").should("contain", "Status: Cancel");
       //Click the "Accept" button and verify the status changes
-      cy.get("@reservationCard").find(".btn").contains("Accept").click();
+      cy.get("@reservationCard").find(".status-buttons .btn").click({force: true});
       cy.get("@reservationCard").should("contain", "Status: Accept");
     });
 
-    it("should show Lecturer text for teacher res", () => {
+   it("should show Lecturer text for teacher res", () => {
         cy.visit('http://localhost:3000/Sign-in');
-        cy.get('input[type="text"]').type('admin@gmail.com');
+        cy.get('input[type="text"]').type('admin@ac.sce.il');
         cy.get('input[type="password"]').type('123456');
         cy.xpath('/html/body/div/div/section/div[2]/form/div[3]/span').click();
         cy.wait(4000); // Wait for four seconds
@@ -64,7 +65,7 @@ describe("Confirmation Page", () => {
       cy.get("@reservationCard").should("contain", "From:");
       cy.get("@reservationCard").should("contain", "Return:");
       cy.get("@reservationCard").should("contain", "Status:");
-      cy.get("@reservationCard").should("contain", "Lecturer");
+      cy.get("@reservationCard").should("contain", "Lecturers reservation");
       
     });
   });
